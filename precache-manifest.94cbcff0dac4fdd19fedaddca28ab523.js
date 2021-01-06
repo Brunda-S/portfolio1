68d103462d59a1ca082d9f252ab4980f61092204
Brunda S <brunda.anju@gmail.com>
2021-01-06 22:25:43 +0530

    Add files via upload

---
+++ precache-manifest.94cbcff0dac4fdd19fedaddca28ab523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd703cd9357246a214264dbc8f8eb76",
+    "url": "/My_Portfolio1/index.html"
+  },
+  {
+    "revision": "0360fb6afbf93ff64bff",
+    "url": "/My_Portfolio1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c353267e7107a272e78",
+    "url": "/My_Portfolio1/static/js/2.e06a6c95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My_Portfolio1/static/js/2.e06a6c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0360fb6afbf93ff64bff",
+    "url": "/My_Portfolio1/static/js/main.fec187b0.chunk.js"
+  },
+  {
+    "revision": "c1d5e063b6bffdbefced",
+    "url": "/My_Portfolio1/static/js/runtime-main.254a3ac8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/My_Portfolio1/static/media/logo.5d5d9eef.svg"
+  }
+]);
